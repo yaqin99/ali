ef03857ada38a7370a010eef7da7c4fb6c5c17d6
yaqin99 <yaqingithub@gmail.com>
2022-09-16 17:51:54 +0700

    Layouting Done

--- lib/views/monitoringPage.dart
+++ lib/views/monitoringPage.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -11,7 +9,7 @@ class MonitoringPage extends StatefulWidget {
 }
 
 class _MonitoringPageState extends State<MonitoringPage> {
-  Color warna = Color.fromRGBO(46, 204, 113, 1);
+  Color warna = const Color.fromRGBO(46, 204, 113, 1);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,24 +31,24 @@ class _MonitoringPageState extends State<MonitoringPage> {
                     Row(
                       children: [
                         Container(
-                          margin: EdgeInsets.only(right: 10),
+                          margin: const EdgeInsets.only(right: 10),
                           width: MediaQuery.of(context).size.width * 0.06,
                           height: MediaQuery.of(context).size.height * 0.03,
                           decoration: BoxDecoration(
                               color: warna,
                               borderRadius: BorderRadius.circular(50)),
                         ),
-                        Text(
+                        const Text(
                           'Connected',
                           style: TextStyle(fontSize: 17),
                         )
                       ],
                     ),
                     Container(
-                      margin: EdgeInsets.only(top: 10),
+                      margin: const EdgeInsets.only(top: 10),
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
+                        children: const [
                           Text(
                             'Monitoring Air',
                             style: TextStyle(
@@ -60,10 +58,10 @@ class _MonitoringPageState extends State<MonitoringPage> {
                       ),
                     ),
                     Container(
-                      margin: EdgeInsets.only(top: 10),
+                      margin: const EdgeInsets.only(top: 10),
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
+                        children: const [
                           Text(
                             'Senin, 17 Agustus 2022 - 07:00',
                             style: TextStyle(
@@ -75,117 +73,8 @@ class _MonitoringPageState extends State<MonitoringPage> {
                   ],
                 ),
               ),
-              Container(
-                  height: MediaQuery.of(context).size.height * 0.35,
-                  width: MediaQuery.of(context).size.width * 0.9,
-                  decoration: BoxDecoration(
-                      color: Color.fromRGBO(237, 245, 241, 1),
-                      borderRadius: BorderRadius.circular(25)),
-                  child: Center(
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Stack(children: [
-                            Center(
-                              child: Container(
-                                height: 200,
-                                width: 200,
-                                decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    borderRadius: BorderRadius.circular(100)),
-                                child: CircularProgressIndicator(
-                                  value: 0.7,
-                                  valueColor:
-                                      new AlwaysStoppedAnimation<Color>(warna),
-                                  strokeWidth: 13,
-                                ),
-                              ),
-                            ),
-                            Center(
-                                child: Padding(
-                              padding: const EdgeInsets.only(top: 80),
-                              child: Text('190 cm',
-                                  style: TextStyle(fontSize: 38)),
-                            ))
-                          ]),
-                          Text(
-                            'Tinggi Air',
-                            style: TextStyle(fontSize: 20),
-                          )
-                        ]),
-                  )),
-              Padding(
-                padding: const EdgeInsets.only(top: 25, bottom: 10),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Penyiraman Otomatis',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ),
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height * 0.2,
-                width: MediaQuery.of(context).size.width * 0.9,
-                decoration: BoxDecoration(
-                    color: Color.fromRGBO(237, 245, 241, 1),
-                    borderRadius: BorderRadius.circular(25)),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text('Waktu'),
-                        Container(
-                          height: MediaQuery.of(context).size.height * 0.04,
-                          child: Column(
-                            children: [
-                              Text('06:00 - 06:15'),
-                              Text('06:00 - 06:15'),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                    Stack(children: [
-                      Center(
-                        child: Container(
-                          height: 120,
-                          width: 120,
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(100)),
-                          child: CircularProgressIndicator(
-                            value: 0.5,
-                            valueColor:
-                                new AlwaysStoppedAnimation<Color>(warna),
-                            strokeWidth: 9,
-                          ),
-                        ),
-                      ),
-                      Center(
-                          child: Padding(
-                        padding: const EdgeInsets.only(top: 60, left: 35),
-                        child: Column(
-                          children: [
-                            Center(
-                                child: Text('1/2',
-                                    style: TextStyle(
-                                        fontSize: 23,
-                                        fontWeight: FontWeight.bold))),
-                            Center(
-                                child: Text('Sehari',
-                                    style: TextStyle(fontSize: 16))),
-                          ],
-                        ),
-                      ))
-                    ]),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 25, bottom: 10),
+              const Padding(
+                padding: EdgeInsets.only(bottom: 10),
                 child: Align(
                   alignment: Alignment.centerLeft,
                   child: Text(
@@ -198,7 +87,7 @@ class _MonitoringPageState extends State<MonitoringPage> {
                 height: MediaQuery.of(context).size.height * 0.2,
                 width: MediaQuery.of(context).size.width * 0.9,
                 decoration: BoxDecoration(
-                    color: Color.fromRGBO(237, 245, 241, 1),
+                    color: const Color.fromRGBO(237, 245, 241, 1),
                     borderRadius: BorderRadius.circular(25)),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -222,25 +111,143 @@ class _MonitoringPageState extends State<MonitoringPage> {
                     Column(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        Text('Status Penyiraman'),
+                        const Text('Status Penyiraman'),
                         Container(
                             height: 80,
                             width: 80,
                             decoration: BoxDecoration(
                                 color: warna,
                                 borderRadius: BorderRadius.circular(100)),
-                            child: Center(
+                            child: const Center(
                                 child: FaIcon(
                               FontAwesomeIcons.faucet,
                               color: Colors.white,
                               size: 50,
                             ))),
-                        Text('Menyiram'),
+                        const Text('Menyiram'),
                       ],
                     )
                   ],
                 ),
               ),
+              const Padding(
+                padding: EdgeInsets.only(top: 25, bottom: 10),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Penyiraman Otomatis',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.2,
+                width: MediaQuery.of(context).size.width * 0.9,
+                decoration: BoxDecoration(
+                    color: const Color.fromRGBO(237, 245, 241, 1),
+                    borderRadius: BorderRadius.circular(25)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        const Text('Waktu'),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.04,
+                          child: Column(
+                            children: const [
+                              Text('06:00 - 06:15'),
+                              Text('06:00 - 06:15'),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                    Stack(children: [
+                      Center(
+                        child: Container(
+                          height: 120,
+                          width: 120,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(100)),
+                          child: CircularProgressIndicator(
+                            value: 0.5,
+                            valueColor: AlwaysStoppedAnimation<Color>(warna),
+                            strokeWidth: 9,
+                          ),
+                        ),
+                      ),
+                      Center(
+                          child: Padding(
+                        padding: const EdgeInsets.only(top: 60, left: 35),
+                        child: Column(
+                          children: const [
+                            Center(
+                                child: Text('1/2',
+                                    style: TextStyle(
+                                        fontSize: 23,
+                                        fontWeight: FontWeight.bold))),
+                            Center(
+                                child: Text('Sehari',
+                                    style: TextStyle(fontSize: 16))),
+                          ],
+                        ),
+                      ))
+                    ]),
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 25, bottom: 10),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Ketinggian Air',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+              Container(
+                  height: MediaQuery.of(context).size.height * 0.35,
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  decoration: BoxDecoration(
+                      color: const Color.fromRGBO(237, 245, 241, 1),
+                      borderRadius: BorderRadius.circular(25)),
+                  child: Center(
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Stack(children: [
+                            Center(
+                              child: Container(
+                                height: 200,
+                                width: 200,
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(100)),
+                                child: CircularProgressIndicator(
+                                  value: 0.7,
+                                  valueColor:
+                                      AlwaysStoppedAnimation<Color>(warna),
+                                  strokeWidth: 13,
+                                ),
+                              ),
+                            ),
+                            const Center(
+                                child: Padding(
+                              padding: EdgeInsets.only(top: 80),
+                              child: Text('190 cm',
+                                  style: TextStyle(fontSize: 38)),
+                            ))
+                          ]),
+                          const Text(
+                            'Tinggi Air',
+                            style: TextStyle(fontSize: 20),
+                          )
+                        ]),
+                  )),
               Padding(
                 padding: const EdgeInsets.only(top: 25, bottom: 10),
                 child: Align(
